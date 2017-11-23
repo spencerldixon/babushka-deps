6ca6f2aaf71d2de545bbc3546d041a82ca204dd5
Spencer Dixon <spencer.dixon@unep-wcmc.org>
2017-11-23 16:02:42 +0000

    Update git.rb

--- scripts/git.rb
+++ scripts/git.rb
@@ -9,29 +9,29 @@ end
 
 dep "set-user-name" do
   met? { shell("git config user.name") == "Spencer Dixon" }
-  meet { shell "git config --global user.name 'Spencer Dixon'" }
+  meet { shell("git config --global user.name 'Spencer Dixon'")}
 end
 
 dep "set-email" do
   met? { shell("git config user.email") == "spencerlloyddixon@gmail.com" }
-  meet { shell "git config --global user.email 'spencerlloyddixon@gmail.com'" }
+  meet { shell("git config --global user.email 'spencerlloyddixon@gmail.com'") }
 end
 
 dep "set-core-editor" do
   met? { shell("git config core.editor") == "vim" }
-  meet { shell "git config --global core.editor 'vim'" }
+  meet { shell("git config --global core.editor 'vim'") }
 end
 
 dep "set-push-default" do
-  met? { shell("git config push.default") == "simple" }
-  meet { shell "git config --global push.default simple" }
+  met? { shell("git config push.default") == "simple") }
+  meet { shell("git config --global push.default simple") }
 end
 
 dep "set-global-gitignore" do
   met? { File.exist?("~/.gitignore_global") }
   meet { 
-    shell "cp ~/.babushka/sources/spencerldixon/gitignore_global.txt ~/.gitignore_global"
-    shell "git config --global core.excludesfile ~/.gitignore_global" 
+    shell("cp ~/.babushka/sources/spencerldixon/gitignore_global.txt ~/.gitignore_global")
+    shell("git config --global core.excludesfile ~/.gitignore_global" )
     }
 end
 
