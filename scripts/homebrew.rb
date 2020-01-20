31970b94c0a74b6d906a3e15e891dee5d4b611f5
Spencer Dixon <spencerlloyddixon@gmail.com>
2020-01-20 11:28:10 +0000

    Fixed typo

--- scripts/homebrew.rb
+++ scripts/homebrew.rb
@@ -1,3 +1,3 @@
 dep 'install-homebrew' do
-  meet? { shell '/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"' }
+  meet { shell '/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"' }
 end
