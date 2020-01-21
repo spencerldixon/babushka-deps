98145fa2e03d6abcd2d9febdbcd8cbc62fb03257
Spencer Dixon <spencerlloyddixon@gmail.com>
2020-01-21 21:16:21 +0000

    Added ssh config

--- scripts/ssh.rb
+++ scripts/ssh.rb
@@ -4,8 +4,11 @@ dep 'generate-ssh-key' do
   met? { system('ls -al ~/.ssh') }
   meet {
     system 'ssh-keygen -t rsa -b 4096 -C "spencerlloyddixon@gmail.com"'
-
     system 'pbcopy < ~/.ssh/id_rsa.pub'
+    system 'eval "$(ssh-agent -s)"'
+    system 'cp ~/.babushka/sources/spencerldixon/.ssh_config ~/.ssh/config'
+    system 'ssh-add -K ~/.ssh/id_rsa'
+
     puts "💻 Your new SSH key has been copied to your clipboard"
   }
 end
