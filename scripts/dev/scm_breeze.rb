8beb92d3bb74025296c9af81017c097a47d32b2c
Spencer Dixon <spencerlloyddixon@gmail.com>
2024-07-09 13:17:45 +0100

    Changed scm breeze from git to https

--- scripts/dev/scm_breeze.rb
+++ scripts/dev/scm_breeze.rb
@@ -2,7 +2,7 @@ dep "scm_breeze" do
   met? { "~/.scm_breeze".p.dir? }
   meet {
     shell "mkdir ~/.scm_breeze"
-    shell "git clone git://github.com/ndbroadbent/scm_breeze.git ~/.scm_breeze"
+    shell "git clone https://github.com/ndbroadbent/scm_breeze.git ~/.scm_breeze"
     shell "~/.scm_breeze/install.sh"
     shell "source ~/.zshrc"
   }
